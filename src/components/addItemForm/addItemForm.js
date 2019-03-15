5192fff28f34054431b885b3efce790e368d65ee
Lindsey Sandberg <sandbergy@yahoo.com>
2019-03-15 08:41:13 -0500

    adds new item form

---
+++ src/components/addItemForm/addItemForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Input from '@material-ui/core/Input';
+import { Button } from '@material-ui/core';
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
+class addItemForm extends Component {
+    state = {
+        newItem:{
+            description: '',
+            url: '',
+            user_id: 0,
+        }
+        }
+
+
+    handleNameChange = (key) => (event) => {
+        console.log('event happended')
+        this.setState({
+            newItem: {
+                ...this.state.newItem,
+                [key]: event.target.value,
+            }
+        });
+    }
+
+    addNewPlant = event => {
+        event.preventDefault();
+        this.props.dispatch({ type: 'POST_SHELF', payload: this.state.newItem })
+        this.setState({
+            newItem: {
+                description: '',
+                url: '',
+                user_id: 0
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>This is the form</h3>
+
+                <form onSubmit={this.addNewItem}>
+                    <Input placeholder='Description' type='text' value={this.state.newItem.description} onChange={this.handleNameChange('description')} />
+                    <Input placeholder='url' type='text' value={this.state.newItem.url} onChange={this.handleNameChange('url')} />
+                    <Input placeholder='user_id' type='text' value={this.state.newItem.user_id} onChange={this.handleNameChange('user_id')} />
+                    <Button type='submit' value='Add New Item'>Add Item</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+export default connect(mapStateToProps)(addItemForm);
